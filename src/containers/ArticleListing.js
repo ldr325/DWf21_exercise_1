8864bc340de893884d8a0925dffd001a7148e870
LeoR <ldr325@nyu.edu>
2021-09-16 01:01:35 -0400

    create containers

---
+++ src/containers/ArticleListing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ArticleListing(){
+  const articlesTitle = `Articles`;
+  return (
+    <div>
+      <h1>{articlesTitle}</h1>
+    </div>
+  );
+}
+
+
+export default ArticleListing;
