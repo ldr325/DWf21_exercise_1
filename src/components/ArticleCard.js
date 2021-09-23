5284b8f537f5a729c47873d5a91f01450f5cd0cb
LeoR <ldr325@nyu.edu>
2021-09-23 08:54:41 -0400

    dates,switch case header

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 function ArticleCard({article}) {
+  const newDate = new Date(article.publishedDate); // tye Date
+  const dateString = newDate.toDateString();
+
   //console.log(article);
   return(
     <div className="ArticleCardWrapper">
@@ -9,10 +12,10 @@ function ArticleCard({article}) {
       </div>
       <div className="ArticleCardText">
         <h2 className="ArticleCardTitle">{article.title}</h2>
-        <p className="ArticleCardDate">{article.publishedDate}</p>
+        <p className="ArticleCardDate">{dateString}</p>
         <p className="ArticleCardBlurb">{article.blurb}</p>
         <p className="ArticleCardLink">
-          <a href="#">Read More</a>
+          <a href={`/articlepage/${article.id}`}>Read More</a>
         </p>
         </div>
     </div>
--- src/containers/ArticlePage.js
+++ src/containers/ArticlePage.js
@@ -12,20 +12,41 @@ function Article() {
     setArticle(dataToSet)
   }, [id]);
 
+  const newDate = new Date(article.publishedDate); // tye Date
+  const dateString = newDate.toDateString();
 
 
   return(
     <main>
-      <section className="ArticleHeader">
-        <h1 className="HeaderOneStyle">{article.title}</h1>
-        <p className="ArticleCardDate">{article.publishedDate}</p>
-        <p className="ArticleHeaderBlurb"> {article.blurb}</p>
+      <section
+        className="ArticleHeader"
+        style={{
+          backgroundImage: `url('${article.image && article.image.url}')`,
+          backgroundPosition: "center",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="ArticleHeaderText">
+          <h1 className="HeaderOneStyle">{article.title}</h1>
+          <p className="ArticleCardDate">{dateString}</p>
+          <p className="ArticleHeaderBlurb"> {article.blurb}</p>
+        </div>
       </section>
       <section className="ArticleText">
         {article.articleText &&
           article.articleText.map((text, i) => {
-            return <p key={i}>{text.data}</p>;
-          })};
+            const type = text.type;
+            switch(type) {
+              case 'p':
+                return <p key={i}>{text.data}</p>;
+              case 'h2':
+                return <h2 key={i}>{text.data}</h2>
+              case 'h3':
+                return <h3 key={i}>{text.data}</h3>
+              default:
+                return <p key={i}>{text.data}</p>;
+            }
+          })}
 
       </section>
 
